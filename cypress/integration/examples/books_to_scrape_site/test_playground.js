2d293d2362d48708bdbe8012f923c310895a2ea0
jameshughes7 <hugjimbo77@gmail.com>
2020-05-20 09:38:15 +0100

    added books_to_scape_site folder

---
+++ cypress/integration/examples/books_to_scrape_site/test_playground.js
@@ -0,0 +1,15 @@
+describe('Browser actions', () => {
+	it('Shoulld load books website', () => {
+		cy.visit('http://books.toscrape.com/index.html', { timeout: 10000 });
+		cy.url().should('include', 'index.html');
+	});
+
+	it('should click on Travel Category', () => {
+		cy.get('a').contains('Travel').click();
+		cy.get('h1').contains('Travel');
+	});
+
+	it('should display correct number of books', () => {
+		cy.get('.product_pod').its('length').should('eq', 11);
+	});
+});
--- cypress/integration/examples/checkout_site/examples/simple.spec.js
+++ cypress/integration/examples/checkout_site/examples/simple.spec.js
@@ -1,13 +1,24 @@
 describe('Browser actions', () => {
     it('should load the correct url', () => {
         cy.visit("https://www.checkout.com/", {timeout: 10000 });
+        cy.url().should('include', 'checkout');
     });
 
-    it('should check the correct url', () => {
-        cy.url().should('include', 'checkout');
+    // explicit wait for 3 seconds
+    it('should wait for 3 seconds', () => {
+        cy.wait(3000);
+    });
+
+    // pause the execution of the tests
+    it('should pause the execution', () => {
+        cy.pause();
     });
 
     it('should check for correct url', () => {
         cy.get('#revolving-text > h1.GeneralStylesc__Heading1-s1al9y-1.cZSkBk').should('be.visible');  
+    });
+
+    it('should click on Know more button', () => {
+        cy.get('#homepage-hero-btn').should('contain.text', 'Know more')
     });
 });
