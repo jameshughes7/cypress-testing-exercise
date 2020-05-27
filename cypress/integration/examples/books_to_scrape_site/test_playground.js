fc8d220fec96b5eddc3ad017c28cd1f844c0b3b3
jameshughes7 <hugjimbo77@gmail.com>
2020-05-27 06:55:47 +0100

    cy logging example

--- cypress/integration/examples/books_to_scrape_site/test_playground.js
+++ cypress/integration/examples/books_to_scrape_site/test_playground.js
@@ -1,7 +1,8 @@
 describe('Browser actions', () => {
 	it('Shoulld load books website', () => {
 		cy.visit('http://books.toscrape.com/index.html', { timeout: 10000 });
-		cy.url().should('include', 'index.html');
+        cy.url().should('include', 'index.html');
+        cy.log('Website Loaded!')
 	});
 
 	it('should click on Travel Category', () => {
@@ -11,5 +12,17 @@ describe('Browser actions', () => {
 
 	it('should display correct number of books', () => {
 		cy.get('.product_pod').its('length').should('eq', 11);
-	});
+    });
+    
+    it('should click on poetry category', () => {
+        cy.get('a').contains('Poetry').click();
+    });
+
+    it('should click on Olio book detail', () => {
+        cy.get('a').contains('Olio').click();
+    });
+
+    it('should have the correct price tag', () => {
+        cy.get('.price_color').contains('23.88');
+    });
 });
