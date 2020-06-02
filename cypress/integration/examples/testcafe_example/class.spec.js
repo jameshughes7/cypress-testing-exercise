e09141be4e391d0abdf0fb0a788689f37424d407
jameshughes7 <hugjimbo77@gmail.com>
2020-06-03 00:00:11 +0100

    before and after hooks example

--- cypress/integration/examples/testcafe_example/class.spec.js
+++ cypress/integration/examples/testcafe_example/class.spec.js
@@ -19,6 +19,27 @@ class HomePage extends BasePage {
 }
 
 describe('Abstraction with classes', () => {
+    before(function() {
+        //runs before all tests inside describe
+        //setup test data or test context
+        //seed or reset the database
+        HomePage.loadHomePage();
+    })
+
+    after(function() {
+        //runs agter all tests inside the describe block are done
+        //test clean up
+        //clean cookies or localstorage
+    })
+
+    beforeEach(function() {
+        //runs before each it block it in the describe
+    })
+
+    afterEach(function() {
+        //runs after each it in the describe
+    })
+
     it('should scroll down and up the page', () => {
         HomePage.loadHomePage();
         HomePage.scrollToBottom();
